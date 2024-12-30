3bb5e47e7d1cc15941749f4185e6da3ecdcf1bbc
Isaac Emmanuel <isaacnsisong@gmail.com>
2024-12-30 02:32:24 +0100

    Changes made on 2024-12-30 02:32:23

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/domains.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
+
+from app import routes, models
